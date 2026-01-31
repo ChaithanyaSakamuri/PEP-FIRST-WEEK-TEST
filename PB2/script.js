0ed65a092e70b0fe47ec17e82b6e92d9b6edc833
Chaithanya Sudheer Sakamuri <153195215+ChaithanyaSakamuri@users.noreply.github.com>
2026-01-31 10:15:24 +0530

    Add files via upload

---
+++ PB2/script.js
@@ -0,0 +1,9 @@
+const heading = document.getElementById("heading");
+
+heading.textContent = "Welcome to JavaScript DOM Manipulation";
+heading.style.color = "white";
+heading.style.backgroundColor = "teal";
+heading.style.textAlign = "center";
+heading.style.padding = "15px";
+
+heading.classList.add("main-heading");
